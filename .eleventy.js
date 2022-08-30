b11e1001b7dc9161d2d3c6def5869bae0759a4b3
Frederik De Bleser <frederik@debleser.be>
2022-08-30 18:09:08 +0200

    Do a per-date sorting.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,13 @@
+const RESEARCH_WEEK_DATES = [
+	['Monday', 10],
+	['Tuesday', 11],
+	['Wednesday', 12],
+	['Thursday', 13],
+	['Friday', 14],
+	['Saturday', 15],
+	['Sunday', 16]
+];
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('static');
 	eleventyConfig.addPassthroughCopy('admin');
@@ -8,4 +18,10 @@ module.exports = function (eleventyConfig) {
 		return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
 		// return .toLocaleDateString();
 	});
+
+	for (const [day, date] of RESEARCH_WEEK_DATES) {
+		eleventyConfig.addCollection(`events${day}`, function (api) {
+			return api.getAll().filter((event) => event.data.date?.getDate() === date);
+		});
+	}
 };
